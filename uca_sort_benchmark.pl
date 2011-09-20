a131a03e8798237a9eca9ae922f9215c8c4dc0d4
Tony Cook <tony@develop-help.com>
2011-09-20 14:56:47 +1000

    add extra case for Unicode::ICU::Collator prototype

--- uca_sort_benchmark.pl
+++ uca_sort_benchmark.pl
@@ -10,15 +10,19 @@ use autodie;
 
 use Benchmark qw(cmpthese);
 use Unicode::Collate::Locale;
+use Unicode::ICU::Collator;
 
 # Get unique list of words from specified file
 my @words = uniq map { split /\b/u } decode_utf8( slurp(shift) );
+
+my $icu = Unicode::ICU::Collator->new("nb");
 
 # Do benchmark
 my $uca = Unicode::Collate::Locale->new( locale => 'nb' );
 cmpthese(1_000, {
     'sort' => sub { my @sorted_words =       sort @words  },
     'uca'  => sub { my @sorted_words = $uca->sort(@words) },
+    'icu'  => sub { my @sorted_words = $icu->sort(@words) },
 });
 
 exit;
